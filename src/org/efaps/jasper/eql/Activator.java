ebd597139d901942c49577a3fda95ee8e971de8c
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-10-14 02:50:48 +0000

    - studio-plugin: adding basic version
    git-svn-id: https://efaps.googlecode.com/svn/eFaps-JasperSoftStudio-plugin/trunk@14229 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/org/efaps/jasper/eql/Activator.java
+++ src/org/efaps/jasper/eql/Activator.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
  * http://www.jaspersoft.com.
- * 
+ *
  * Unless you have purchased  a commercial license agreement from Jaspersoft,
  * the following license terms  apply:
- * 
+ *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,56 +21,56 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractJRUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.efaps.jasper.eql"; //$NON-NLS-1$
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.efaps.jasper";
 
-	// The shared instance
-	private static Activator plugin;
+    // The shared instance
+    private static Activator plugin;
 
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+    /**
+     * The constructor
+     */
+    public Activator() {
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-	 * )
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+     * )
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-	 * )
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+     * )
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
 
-	/**
-	 * Returns the shared instance
-	 * 
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
 
-	@Override
-	public String getPluginID() {
-		return PLUGIN_ID;
-	}
+    @Override
+    public String getPluginID() {
+        return PLUGIN_ID;
+    }
 
 }
